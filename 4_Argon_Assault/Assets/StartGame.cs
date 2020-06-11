55ba287450961a40aa916485eb884ef49e3dd0fd
Karenmax11 <karen.nikoghosyan11@gmail.com>
2020-06-11 14:09:00 +0300

    8 Setup A Splash Screen

---
+++ 4_Argon_Assault/Assets/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+      
+    }
+    
+}
