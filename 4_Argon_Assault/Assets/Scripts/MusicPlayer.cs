9a9b8538f7b624ab9b5c1611fef9c47c57fda668
Karenmax11 <karen.nikoghosyan11@gmail.com>
2020-06-20 17:07:08 +0300

    25 Singleton Pattern Without Statics

--- 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -4,6 +4,15 @@ public class MusicPlayer : MonoBehaviour
 {
     private void Awake()
     {
-        DontDestroyOnLoad(this);
+
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
