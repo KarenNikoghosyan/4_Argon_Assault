3ae163bbbaabc76d1d9eacb511ed177dac089297
Karenmax11 <karen.nikoghosyan11@gmail.com>
2020-06-21 16:22:29 +0300

    28 Instantiating At Runtime

--- 4_Argon_Assault/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,8 @@ public class Enemy : MonoBehaviour
     }
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
